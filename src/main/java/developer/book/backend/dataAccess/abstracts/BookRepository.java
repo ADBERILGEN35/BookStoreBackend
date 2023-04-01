a833e2adf93d494c90e220e0e815457e177dc660
ADBERILGEN35 <91200001205@ogrenci.ege.edu.tr>
2023-04-01 23:14:15 +0300

    BookBackend || Repository update.

--- src/main/java/developer/book/backend/dataAccess/abstracts/BookRepository.java
+++ src/main/java/developer/book/backend/dataAccess/abstracts/BookRepository.java
@@ -2,6 +2,7 @@ package developer.book.backend.dataAccess.abstracts;
 
 import developer.book.backend.entities.concretes.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -10,5 +11,9 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Query("SELECT r FROM Book r WHERE r.isbn LIKE %:isbn%")
     List<Book> findByIsbn(String isbn);
+
+    @Modifying
+    @Query("DELETE FROM Book b WHERE b.title IS NULL OR b.authors IS EMPTY OR b.isbn IS NULL OR b.publisher IS NULL OR b.publishedDate IS NULL OR b.pageCount IS NULL OR b.description IS NULL OR b.imageLinks IS NULL")
+    void deleteBooksIfAnyFieldIsNull();
 
 }
